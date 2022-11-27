39774396f1b781085ff22baf108c20be9eb906f1
Adila Bendahou <adilabendahou98@gmail.com>
2022-11-27 01:01:49 +0100

    Redux Setup

--- src/componenets/Movies/Movies.jsx
+++ src/componenets/Movies/Movies.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 function Movies() {
+  const { data } = useGetMoviesQuery();
+  console.log(data);
   return (
     <div>Movies</div>
   );
