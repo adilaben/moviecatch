66e1add921e4756c258f422f78ec93926acfd97d
Adila Bendahou <adilabendahou98@gmail.com>
2022-11-27 19:55:06 +0100

    Custom ScrollBar

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import App from './componenets/App';
 import store from './app/store';
+import './index.css';
 
 const theme = createTheme({});
 
